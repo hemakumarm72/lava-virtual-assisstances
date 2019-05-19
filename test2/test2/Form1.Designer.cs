1032ac3c6b5c480754df431ea738896127e96f9c
Hema kumar <41264163+hemakumarm72@users.noreply.github.com>
2019-05-19 12:30:40 +0530

    Add files via upload

---
+++ test2/test2/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace test2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axActiveHaptekX1 = new AxACTIVEHAPTEKXLib.AxActiveHaptekX();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axActiveHaptekX1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axActiveHaptekX1
+            // 
+            this.axActiveHaptekX1.Enabled = true;
+            this.axActiveHaptekX1.Location = new System.Drawing.Point(4, 3);
+            this.axActiveHaptekX1.Name = "axActiveHaptekX1";
+            this.axActiveHaptekX1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axActiveHaptekX1.OcxState")));
+            this.axActiveHaptekX1.Size = new System.Drawing.Size(330, 242);
+            this.axActiveHaptekX1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 251);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(330, 100);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(4, 357);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(330, 100);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(223, 477);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Getresponse";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(334, 559);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.axActiveHaptekX1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axActiveHaptekX1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxACTIVEHAPTEKXLib.AxActiveHaptekX axActiveHaptekX1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+
+
+    }
+}
+
---
+++ test2/test2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AIMLbot;
+
+namespace test2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            axActiveHaptekX1.HyperText = @"\load [file= standardstartup.hap]";
+
+             
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bot ai = new Bot(); // the named our bot this code
+            ai.loadSettings(); // write this code you will load setting frame
+            ai.loadAIMLFromFiles(); // with this code you will load aiml files into your project
+            ai.isAcceptingUserInput = false; //write the help of 
+            User myuser = new User("Larva", ai );
+            ai.isAcceptingUserInput = true;
+            Request r = new Request(textBox1.Text, myuser, ai);
+            Result res = ai.Chat(r);
+            textBox2.Text =  res.Output;
+            axActiveHaptekX1.HyperText = @"\speak [string= ["+textBox2.Text+"]]";
+
+
+        }
+    }
+}
